650cfa78007e0e0e52f1c1fa8a5202092a17a066
Chinedu <116736707+SimpleNedu@users.noreply.github.com>
2023-01-09 17:30:07 +0100

    good going yet, still learning node basics

--- index2.js
+++ index2.js
@@ -1,41 +1,48 @@
+// note that the fs part helps me async promisify the first readfile action and no error was handled here as this tutorial was more about learning node than handling errors
 const fs = require('fs').promises
 const fss = require('fs')
 const http = require('http')
 const url = require('url')
+const replaceTemplates = require('./modules/replaceTemplates')
+
 
 //  functions that need to be called once throuoghout the run time of the nodeserver should be listed as a op level function asthis will help make the 
 // reduce the workload on the create server function
 const data = async() =>{
     return await fs.readFile('./dev-data/data.json', 'UTF-8')
 }
-const dataResponse = data()
+const dataResponse = data().then((res)=>JSON.parse(res))
 
-/////////////////////////////////////////////////////////
-// server setup
-// for trhe html templates added below, the same theory as above hold to prvent repitition ad stress on the system that has to reload the html files on every reload
 const tempOverview = fss.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const tempCard = fss.readFileSync(`${__dirname}/templates/templateCard.html`, 'utf-8');
 const tempProduct = fss.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 
+// server setup
 const server = http.createServer((req, res)=>{
-    const pathName = req.url;
+    const  {query, pathname} = url.parse(req.url, true)
 
     // overview page
-    if (pathName === '/' || pathName === '/overview'){
+    if (pathname === '/' || pathname === '/overview'){
         res.writeHead(200, {"content-type": "text/html"})
-        res.end(tempOverview)
+        dataResponse.then((res)=>{
+            const cardsHtml = res.map(el => replaceTemplates(tempCard, el)).join('')
+            theTemp = tempOverview.replace(/{%PRODUCTS_CARDS%}/g, cardsHtml)
+            return theTemp
+        }).then((resp)=>{
+            res.end(resp)
+        })
 
     // product page
-    } else if (pathName === '/product') {
-        res.end('we are at the product section')
-
-
+    } else if (pathname === '/product') {
+        res.writeHead(200, {"content-type": "text/html"})
+        
+        dataResponse.then(res=>res.filter(resp=>query.id== resp.id)) 
+        .then(([reps])=>replaceTemplates(tempProduct, reps))
+        .then((reps)=>res.end(reps))
+        
     // api page
-    } else if (pathName === '/api') {
-        dataResponse.then((response)=>{
-            const data = JSON.parse(response)
-            res.end(JSON.stringify(data))
-        })
+    } else if (pathname === '/api') {
+        dataResponse.then((resp)=>res.end(JSON.stringify(resp)))
         .catch((err)=>{
             res.writeHead(404, {
                 "content-type": "text/html",
